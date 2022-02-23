f62f499350cd6990ef05be0875fe7b71ee163464
BiGTAD <98679333+BiGTAD@users.noreply.github.com>
2022-02-23 19:33:30 +0700

    Update alien.php

--- alien.php
+++ alien.php
@@ -1,6 +1,6 @@
 {
-  "Version_update": "1.0",
-  "Link_zip": "https://github.com/BiGTAD/alien/blob/main/21-02-65.zip",
+  "Version_update": "1.01",
+  "Link_zip": "https://github.com/BiGTAD/alien/blob/main/21-02-65.zip?raw=true",
   "Password": "alien88",
   "Message": "ALIEN VPN
 ใส่รายละเอียด
