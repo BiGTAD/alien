8de8061bfebddfc5785ef62bda95d3121e151504
BiGTAD <98679333+BiGTAD@users.noreply.github.com>
2022-02-26 16:16:21 +0700

    Create boxing.php

---
+++ boxing.php
@@ -0,0 +1,7 @@
+{
+  "Version_update": "1.00",
+  "Link_zip": "https://github.com/BiGTAD/alien/blob/main/26-02-65.zip?raw=true",
+  "Password": "boxing88",
+  "Message": "BoxingVPN
+ใส่รายละเอียด
+"}
