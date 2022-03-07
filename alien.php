472f32dc665a8d58d68ac93f9f49585840dffed5
BiGTAD <98679333+BiGTAD@users.noreply.github.com>
2022-03-07 22:47:50 +0700

    Delete alien.php

--- alien.php
+++
@@ -1,9 +0,0 @@
-{
-  "Version_update": "1.01",
-  "Link_zip": "https://github.com/BiGTAD/alien/blob/main/21-02-65.zip?raw=true",
-  "Password": "alien88",
-  "Message": "ALIEN VPN
-ใส่รายละเอียด
-
-
-"}
