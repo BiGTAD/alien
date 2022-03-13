ac2528779fb0c2b8522ad603180853f3cbbc9bee
BiGTAD <98679333+BiGTAD@users.noreply.github.com>
2022-03-13 18:57:50 +0700

    Update boxing.php

--- boxing.php
+++ boxing.php
@@ -1,13 +1,13 @@
 {
-  "Version_update": "2.4",
+  "Version_update": "1.4",
   "Link_zip": "https://github.com/BiGTAD/alien/blob/main/14-03-65.zip?raw=true",
   "Password": "boxing88",
   "Message": "BoxingVPN
-  Update 14/03/65 เซิร์ฟเวอร์ล่าสุด
+Update 14/03/65 เซิร์ฟเวอร์ล่าสุด
 - เพิ่มเซิฟ TH 2 เซิฟเวอร์
 - ปิดเซิฟเวอร์ SG ชั่วคราว
 
-  อ่านละเอียดก่อนใช้งาน
+อ่านละเอียดก่อนใช้งาน
 - TH 2 เซิฟเวอร์ ทั่วไป+ดาวน์โหลด
 - THGAME 4 เซิฟเวอร์ สำหรับเล่นเกมส์
 
